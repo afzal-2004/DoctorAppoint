c30e5f5302cf20648f2defb3b69797b699fd5a67
afzal-2004 <moa44468@gmail.com>
2024-09-17 21:20:44 +0530

    Change project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { Exercises } from "./pages/Exercises/Exercises";
-import { Home } from "./pages/Home/Home";
 function App() {
-  return (
-    <>
-      <Home />
-      <Exercises />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
