c070d0b4f53b13f4f51ae5a22326f8654e8d8b3e
afzal-2004 <moa44468@gmail.com>
2024-09-29 01:46:59 +0530

    Add Cancel button Functionality

--- src/Components/MyAppointments.jsx
+++ src/Components/MyAppointments.jsx
@@ -3,14 +3,14 @@ import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const MyAppointments = () => {
+  const [Cancel, setCancel] = useState(false);
   const { AppointmentsDoctorsid, NextSevenBookingDate, Time, day } =
     useContext(AppContext);
-  console.log(AppointmentsDoctorsid);
+
   const [AppointedDoc, setAppointedDoc] = useState([]);
-  console.log("Appointed Doctor data is : ", AppointedDoc);
+
   useEffect(() => {
     axios.get("/Doctor.json").then((res) => {
-      console.log(res.data);
       const Doctordata = res.data;
       const Doctors = Doctordata?.filter((doc) =>
         AppointmentsDoctorsid.includes(doc._id)
@@ -20,51 +20,70 @@ export const MyAppointments = () => {
   }, [AppointmentsDoctorsid]);
 
   return (
-    <div className=" mt-[8vh]">
-      <h1 className=" text-[25px] font-bold text-center">My appointments </h1>
-      {AppointedDoc.map((doctor, i) => (
-        <div key={i} className="   sm:flex">
-          <div className="  mt-[3vh] sm:m-[3vh]  sm:flex  gap-3 border   w-full  border-slate-400 p-2 rounded-md">
-            <img
-              src={`${doctor.image}`}
-              alt="Doctorimage"
-              className="   object-cover  bg-blue-500 sm:max-w-[200px]   rounded-xl p-0  "
-            />
+    <>
+      <div className=" mt-[8vh]">
+        <h1 className=" text-[25px] font-bold text-center">My appointments </h1>
+        {AppointedDoc.map((doctor, i) => (
+          <div key={i} className="   sm:flex">
+            <div className="  mt-[3vh] sm:m-[3vh]  sm:flex  gap-3 border   w-full  border-slate-400 p-2 rounded-md">
+              <img
+                src={`${doctor.image}`}
+                alt="Doctorimage"
+                className="   object-cover  bg-blue-500 sm:max-w-[200px]   rounded-xl p-0  "
+              />
 
-            <div className="  sm:flex justify-between   w-full">
-              <div className=" mt-[5vh] sm:mt-[0vh]">
-                <h1 className=" text-[20px] font-semibold text-red-500">
-                  {doctor.name}
-                </h1>
-                <span className=" text-[17px] font-light">
-                  {doctor.speciality}
-                </span>
-                <p className=" text-[15px] font-bold">Address :</p>
-                <p className=" text-[18px font-semibold]">
-                  <span>{doctor.address?.line1}</span>
-                  <span>{doctor.address?.line2}</span>
-                </p>
+              <div className="  sm:flex justify-between   w-full">
+                <div className=" mt-[5vh] sm:mt-[0vh]">
+                  <h1 className=" text-[20px] font-semibold text-red-500">
+                    {doctor.name}
+                  </h1>
+                  <span className=" text-[17px] font-light">
+                    {doctor.speciality}
+                  </span>
+                  <p className=" text-[15px] font-bold">Address :</p>
+                  <p className=" text-[18px font-semibold]">
+                    <span>{doctor.address?.line1}</span>
+                    <span>{doctor.address?.line2}</span>
+                  </p>
 
-                <p>Date & Time</p>
-                <span>
-                  {`${NextSevenBookingDate[day]?.dayName} ${NextSevenBookingDate[day]?.dayDate}`}{" "}
-                  | {doctor.apointmentTime[Time]}
-                </span>
-                <p>Fees</p>
-                <span>{doctor.fees}$</span>
-              </div>
-              <div className="  mt-[5vh] sm:mt-0 flex   sm:flex-col items-center  justify-center sm:justify-end gap-5">
-                <button className=" border border-slate-500 px-3 py-2 w-[150px]">
-                  Cancel
-                </button>
-                <button className=" border border-slate-500 px-3 py-2 w-[150px] bg-blue-500 text-white ">
-                  Pay Here ..
-                </button>
+                  <p>Date & Time</p>
+                  <span>
+                    {`${NextSevenBookingDate[day]?.dayName} ${NextSevenBookingDate[day]?.dayDate}`}{" "}
+                    | {doctor.apointmentTime[Time]}
+                  </span>
+                  <p>Fees</p>
+                  <span>{doctor.fees}$</span>
+                </div>
+                <div className="  mt-[5vh] sm:mt-0 flex   sm:flex-col items-center  justify-center sm:justify-end gap-5">
+                  <button
+                    className=" border border-slate-500 px-3 py-2 w-[150px]"
+                    onClick={() => {
+                      setCancel(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                  <button className=" border border-slate-500 px-3 py-2 w-[150px] bg-blue-500 text-white ">
+                    Pay Here ..
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div>{Cancel && <CancelPopup />}</div>
+    </>
+  );
+};
+const CancelPopup = () => {
+  return (
+    <>
+      <div className=" bg-white  w-full  m-auto  sm:w-[300px] p-2">
+        <h1>Are you Sure !! Cancel Appointment</h1>
+        <button className=" bg-red-400 px-3 py-2">Yes</button>
+        <button className=" bg-green-400 px-3 py-2">Cancel </button>
+      </div>
+    </>
   );
 };
